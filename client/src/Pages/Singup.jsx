72ee447fc1b95b8ddbad576188af126905b959bf
Harsh saxena <harshsaxena427@gmail.com>
2024-04-01 21:54:23 +0530

    complete signup

--- client/src/Pages/Singup.jsx
+++ client/src/Pages/Singup.jsx
@@ -1,9 +1,59 @@
+import { Button, Label, TextInput } from 'flowbite-react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Singup() {
   return (
-    <div>
-      
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+        {/* left */}
+        <div className='flex-1'>
+          <Link to='/' className='font-bold dark:text-white text-4xl'>
+            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+              Harsh's
+            </span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5">This website is made only for project purpose just to learn more in this field</p>
+        </div>
+        {/* Right */}
+        <div className="flex-1">
+          <form className='flex flex-col gap-4'>
+            <div>
+              <Label value='Your username'/>
+              <TextInput
+              type='text'
+              placeholder='Username'
+              id='username'/>
+            </div>
+            <div>
+              <Label value='Your email'/>
+              <TextInput
+              type='text'
+              placeholder='Email'
+              id='email'/>
+            </div>
+            <div>
+              <Label value='Your password'/>
+              <TextInput
+              type='password'
+              placeholder='Password'
+              id='password'/>
+            </div>
+            <Button gradientDuoTone='purpleToPink' type='submit'>
+              Sign Up
+            </Button>
+          </form>
+          <div className='flex gap-2 text-sm mt-5'>
+            <span>
+              Have an account?
+            </span>
+            <Link to='/signin' className='text-blue-500'>
+              Sign in
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
